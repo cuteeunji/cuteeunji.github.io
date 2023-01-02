2e0dd8bbc0359b65e9ae52caeb1aaaf42b7ae293
eunji yeo <cuteeunji@gmail.com>
2023-01-02 22:36:06 +0900

    Add function to delete comments with password

--- js/comments.js
+++ js/comments.js
@@ -8,11 +8,12 @@ const auth7 = "E2k2tKHsw";
 
 const githubUrl = "https://api.github.com/repos/cuteeunji/cuteeunji.github.io";
 
-loadComments(auth1 + auth2 + auth3 + auth4 + auth5 + auth6 + auth7);
-registerComment(auth1 + auth2 + auth3 + auth4 + auth5 + auth6 + auth7);
+var auth = auth1 + auth2 + auth3 + auth4 + auth5 + auth6 + auth7;
+loadComments();
+registerComment();
 moreComment();
 
-function loadComments(auth) {
+function loadComments() {
   fetch(githubUrl, {
     method: "GET",
     headers: {
@@ -40,28 +41,32 @@ function loadComments(auth) {
               $('#guest_more_btn').show();
             }
             commentList.innerHTML += `
-            <li class="comment" style="display: ${display}">
+            <li class="comment" id="comment_${comments[i].number}" style="display: ${display}">
               <p class="name">${comments[i].title}</p>
               <p class="date">${comments[i].created_at
                 .replace("T", "  ")
                 .replace("Z", "")
                 .slice(0, -9)}</p>
               <p class="memo">${comments[i].body}</p>
+              <p class="del"><a href="#none" onclick="javascript:guestbook_del('${comments[i].number}', '${comments[i].labels[0].name}');">삭제</a></p>
             </li>`;
           }
         });
       });
 }
 
-function registerComment(auth) {
+function registerComment() {
   let commentRegistration = document.getElementById("comment-registration");
   commentRegistration.addEventListener("click", () => {
     let name = document.getElementById("comment-name");
     let msg = document.getElementById("comment-msg");
+    let pwd = document.getElementById("comment-pwd");
     if (!name.value) {
       alert("이름을 입력해주세요!");
     } else if (!msg.value) {
       alert("축하 메시지를 입력해주세요!");
+    } else if (!pwd.value) {
+      alert("비밀번호를 입력해주세요!");
     } else {
       fetch(
         githubUrl + "/issues",
@@ -74,21 +79,24 @@ function registerComment(auth) {
           body: JSON.stringify({
             title: name.value,
             body: msg.value,
+            labels: new Array(pwd.value,),
           }),
         }
       ).then((res) => res.json())
       .then((comment) => {
         name.value = "";
         msg.value = "";
+        pwd.value = "";
         let commentList = document.getElementById("comment-list");
         commentList.innerHTML = `
-        <li class="comment">
+        <li class="comment" id="comment_${comments[i].number}">
           <p class="name">${comment.title}</p>
           <p class="date">${comment.created_at
             .replace("T", "  ")
             .replace("Z", "")
             .slice(0, -9)}</p>
           <p class="memo">${comment.body}</p>
+          <p class="del"><a href="#none" onclick="javascript:guestbook_del('${comment.number}', '${comment.labels[0].name}');">삭제</a></p>
         </li>` + commentList.innerHTML;
       });
     }
@@ -109,4 +117,29 @@ function moreComment() {
         $('#guest_more_btn').fadeOut('slow')
     }
   });
+}
+
+var guestbook_del = function(issue_num, issue_pwd){
+  var password = prompt('비밀번호를 입력해주세요!');
+  if (issue_pwd == password) {
+    fetch(
+      githubUrl + "/issues/" + issue_num,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "token " + auth,
+        },
+        body: JSON.stringify({
+          state: 'closed',
+        }),
+      }
+    ).then((res) => res.json())
+    .then((comment) => {
+      var id = '#comment_' + issue_num
+      $(id).remove();
+    });
+  } else {
+    alert('비밀번호가 틀렸어요!')
+  }
 }
